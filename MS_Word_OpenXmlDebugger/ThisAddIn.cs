210148692291a2a8139c49c6a24c6fea2638420c
Alex Butenko <butenko.alexey@gmail.com>
2020-06-02 23:58:04 +0800

    Add project files.

---
+++ MS_Word_OpenXmlDebugger/ThisAddIn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OfficeTools {
+    public partial class ThisAddIn {
+        void ThisAddIn_Startup(object sender, EventArgs e) {
+        }
+
+        void ThisAddIn_Shutdown(object sender, EventArgs e) {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        void InternalStartup() {
+            Startup += ThisAddIn_Startup;
+            Shutdown += ThisAddIn_Shutdown;
+        }
+
+        #endregion
+    }
+}
